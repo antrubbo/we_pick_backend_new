61692f0d7ffcea33945e455e0f0e061552173a41
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-26 18:45:40 -0500

    fixed search and /:id

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const movies = express.Router()
 const axios = require('axios');
+
+require('dotenv').config()
 const KEY = process.env.KEY
 
 movies.get("/", (req, res) => {
@@ -8,11 +10,17 @@ movies.get("/", (req, res) => {
 })
 
 movies.get("/search", (req, res) => {
-    axios.get("https://api.themoviedb.org/3/search/movie?api_key=99fdd78beedc847a99f420187e092842&language=en-US&query=matrix&page=1&include_adult=false")
-    .then(response => {
-        console.log(response.data.results)
-        res.send(response.data.results)
-    })
+    const { title } = req.query
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&query=${title}&page=1&include_adult=false`)
+    .then(response => { res.send(response.data.results)})
+    .catch(response => response.status(422).json( {error: "No results to display!"} ))
+})
+
+movies.get("/:id", (req, res) => {
+    const { id } = req.params
+    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=en-US&append_to_response=videos`)
+    .then(response => res.send(response.data))
+    .catch(response => response.status(404).json( {error: "No movie with that ID!"} ))
 })
 
 module.exports = movies
